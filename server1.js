dea65c6fe6a1b2b46355deed699e480e6f955627
MI7 <muhammadidris204@gmail.com>
2024-05-31 15:11:34 +0400

    added a static folder

--- server1.js
+++ server1.js
@@ -14,6 +14,7 @@ app.use((req, res, next) => {
   );
   next();
 });
+app.use(express.static(__dirname + "/static"))
 
 const { MongoClient, ObjectId } = require("mongodb");
 const client = new MongoClient(
